874f531e7c54107b1b6cd59fce7ef05dccdf7dbf
helder mabasso <hfmabasso@gmail.com>
2024-07-10 01:32:18 +0200

    solving the other express routes exercises using lowdb

--- http_server.js
+++ http_server.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const lodashId = require('lodash-id')
 const app     = express();
 var low     = require('lowdb');
 var fs      = require('lowdb/adapters/FileSync');
 var adapter = new fs('db.json');
 var db      = low(adapter);
+
+db._.mixin(lodashId);
 
 // configure express to serve static files from public directory
 // ------------------------------------------------------------------
@@ -25,11 +28,12 @@ app.get('/data', function(req, res){
 // ----------------------------------------------------
 app.get('/posts/:title/:id/:published', function(req, res){
 
-    var post = {
+    const post = {
         'id' : req.params.id,
         'title' : req.params.title,
         'published' : req.params.published,
     }
+
     db.get('posts').push(post).write();
     console.log(db.get('posts').value());
     res.send(db.get('posts').value());
@@ -41,9 +45,9 @@ app.get('/posts/:title/:id/:published', function(req, res){
 //      curl http://localhost:3000/published/true
 // ----------------------------------------------------
 app.get('/published/:boolean', function(req, res){
-
     // YOUR CODE
-
+    let response = db.get('posts').value().filter(publish => publish.published === true)
+    res.send(response)
 });
 
 // ----------------------------------------------------
@@ -53,6 +57,13 @@ app.get('/published/:boolean', function(req, res){
 app.get('/published/:id/:boolean', function(req, res){
 
     // YOUR CODE
+    let postId = req.params.id
+    let published = req.params.boolean
+    let valuesToUpdate = {
+        'published': published === 'true'
+    }
+    db.get('posts').updateById(req.params.id,valuesToUpdate).write()
+    res.send('Object successfully update')
 
 });
 
@@ -61,9 +72,12 @@ app.get('/published/:id/:boolean', function(req, res){
 //      curl http://localhost:3000/delete/5
 // ----------------------------------------------------
 app.get('/delete/:id/', function(req, res){
-
     // YOUR CODE
-
+    let itemToRemove = db.get('posts').getById(req.params.id).value()
+    console.log(`The object will be removed`);
+    db.get('posts').removeById(req.params.id).write()
+    res.send("Object successfully removed")
+   
 });
 
 // start server
